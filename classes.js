9de8033f1d9cb5ea172b5cc3aeae9cb477c78cca
TimothyHoytBSME <timothyhoytbsme@gmail.com>
2023-02-12 22:19:40 -0600

    better balance

---
+++ classes.js
@@ -0,0 +1,221 @@
+class Piece {
+    constructor(i, j, type) {
+        this.type = type
+        this.cost = 0;
+        this.subtype = "basic"
+        this.color = [40,40,40]
+        this.left = 0;
+        this.top = 0;
+        this.px = i;
+        this.py = j;
+        this.isSelected = false;
+        this.isTarget = false;
+        this.health = 100;
+        this.enemies = new Array([-1]);
+        this.range = 4;
+        this.numLasers = 1;
+        this.power = 1
+
+        this.draw = () => {
+            
+            this.color = (this.type=="base")? [126*(1-this.health/100),0,123*this.health/100] : (this.type=="defense")? [160,160,160] : (this.type=="path")? [123, 123, 123]  : [40,40,40]
+            var col = this.isSelected? [0,123,0] : this.isTarget? [123,123,0] : this.color
+            
+            fillRec([this.left, this.top, pieceSize, pieceSize], colText(col));
+            
+            if(this.type == "base"){
+                fillRec([this.left, this.top+pieceSize*0.9, pieceSize*this.health/100, pieceSize*0.1], colText([123,255,0]))
+                // strokeCir([this.left+pieceSize/2, this.top+pieceSize/2, pieceSize*this.range], 3, colText(col))
+
+            }
+            if(this.type == "defense"){
+                var col = (this.subtype == "basic")? [100,100,0] : (this.subtype == "slow")? [0,100,100] : [255,255,255]
+                fillCir([this.left+pieceSize/2, this.top+pieceSize/2, pieceSize*0.75/2], colText(col))
+                // strokeCir([this.left+pieceSize/2, this.top+pieceSize/2, pieceSize*this.range], 3, colText(col))
+            }
+        }
+        this.drawLaser = ()=>{
+            //todo find nearest enemy and qualify
+            //todo draw line to enemy
+            this.enemies = new Array([-1])
+
+            
+            var sor = [this.left+pieceSize/2, this.top + pieceSize/2]
+            
+            for(var li = 0; li<this.numLasers; li++){
+                var tarPind = 1000000
+                var closest = 1000000;
+                for(var ei=(activeEnemies - 1); ei>=0; ei--){
+                    if(!this.enemies.includes(ei)){
+                        var tartar = [theEnemies[ei].left+pieceSize/4, theEnemies[ei].top + pieceSize/4]
+                        var dis = dist(sor,tartar)
+                        
+                        if(dis <= this.range*pieceSize){
+                            //enemy found
+                            if(this.subtype == "slow"){
+                                if(dis<closest){
+                                    closest = dis
+                                    this.enemies[li] = ei
+                                    // console.log(dis, this.range, pieceSize)
+
+                                }
+                            }else{
+                                if(tarPind > theEnemies[ei].pathStep){
+                                    this.enemies[li] = ei
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if(this.enemies[0] > -1){
+
+                for(var ei=this.enemies.length -1; ei>=0; ei--){
+                    if(this.enemies[ei]<0){
+                        console.warn(this.enemies[ei])
+                    }else if(this.enemies[ei] > (theEnemies.length-1)){
+                        console.warn(this.enemies[ei])
+                    }
+                    var theenemy = theEnemies[this.enemies[ei]]
+                    var tar = [theenemy.left+pieceSize/4, theenemy.top + pieceSize/4]
+                    var sz = 3; var co = [200,180,0,0.8]
+                    if(this.type == "base"){
+                        sz = 5; co = [200,123,50,0.8]
+                    }
+                    if(this.subtype == "slow"){
+                        co = [0,200,200,0.8]
+                    }
+                    drawLine(sor,tar,sz,colText(co))
+                    
+                    if(this.subtype == "basic"){
+                        if(theenemy.health > this.power){
+                            theenemy.health -= (this.power-theenemy.armor >0)? (this.power - theenemy.armor) : 0
+                        }else{
+                            theenemy.health = 0
+                            theenemy.destroy()
+                            score+=theenemy.reward
+                            gold+=theenemy.reward
+                            if(ei > 0){
+                                this.enemies.pop()
+                            }else{
+                                this.enemies[0] = -1
+                            }
+                        }
+                    }else if(this.subtype == "slow"){
+                        theenemy.speedMod /= (this.power+1.25);
+                    }
+                }
+                
+                
+
+            }
+        }
+    }
+}
+
+class Enemy { 
+    constructor(type, pathnum){
+        this.type = type
+        this.color = [123,0,0]
+        this.speed = 0.03;
+        this.left = 50;
+        this.top = 50;
+        this.from = [-1,-1]
+        this.to  = [-1,-1]
+        this.health = 100;
+        this.armor = 0.5;
+        this.damage = 10; 
+        this.pathNum = pathnum;
+        this.reward = 1;
+        // console.log(paths)
+        // console.log(pathnum)
+        this.pathStep = paths[pathnum].length -1
+        this.visible = false;
+        this.speedMod = 1;
+
+        this.draw = ()=>{
+            if((this.to[0]<0)||(this.from[0]<0)){
+                if(this.visible){
+                    this.visible = false
+                }
+            }else{
+                this.visible = true;
+            }
+
+            if(this.visible){
+                fillRec([this.left, this.top, pieceSize/2, pieceSize/2], colText(this.color));
+                fillRec([this.left, this.top+pieceSize/2*0.9, (pieceSize/2)*this.health/100, pieceSize/2*0.1], colText([123,255,0]))
+            }
+
+        }
+        this.move = ()=>{
+            var dir = [ this.to[0]-this.from[0] , this.to[1] - this.from[1]  ]
+            var spDir = dir
+            if(verticalOrien){
+                spDir = [ -spDir[1], spDir[0]]
+            }
+
+            this.left= this.left + this.speed*spDir[0]*pieceSize*this.speedMod
+            this.top= this.top + this.speed*spDir[1]*pieceSize*this.speedMod
+            this.speedMod = 1;
+
+            if(this.to[0]>-1){
+                var destPiece = gameGrid[this.to[0]][this.to[1]]
+                var pastLeft = spDir[0]*((destPiece.left + pieceSize/2) - (this.left + pieceSize/4)) <= 0
+                var pastTop = spDir[1]*((destPiece.top + pieceSize/2) - (this.top + pieceSize/4)) <= 0
+
+                if(pastLeft){
+                    this.left = destPiece.left + pieceSize/4
+                }
+                if(pastTop){
+                    this.top = destPiece.top + pieceSize/4
+                }
+
+                if(pastLeft&&pastTop){
+        
+                    this.pathStep = this.pathStep - 1;
+                    if(this.pathStep < - 0){
+                        //hit base
+                        if(theBase.health > this.damage){
+                            theBase.health-= this.damage
+                        }else{
+                            theBase.health = 0
+                            waveRunning = false;
+                            gameOver = true;
+
+                        }
+
+
+                        this.destroy()
+                    }else{
+                        
+                        this.from = [...this.to]
+                        this.to = [...paths[this.pathNum][this.pathStep]]
+                    }
+                    
+                }
+                // console.log('moving', dir, this.speed, this.left, this.top)
+            }
+            
+        }
+        this.destroy = ()=>{
+            this.speed = 0;
+            this.to = [-1,-1]
+            this.from = [-1,-1]
+            this.left = 0;
+            this.top = 0;
+            activeEnemies--
+            theEnemies.splice(theEnemies.indexOf(this),1)
+            // console.log("enemy removed", theEnemies)
+            if(theEnemies.length == 0){
+                waveNum++
+                genEnemies()
+                eDelayCount = 0
+                sDelayCount = 0;
+
+            }
+        }
+
+    }
+}
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ var gold = startGold;
 var numEnemies = 100;
 var theEnemies = [];   //gets calculated
 var enemyDelay = 50;
-var startDelay = 500; 
+var startDelay = 100; //500
 var eDelayCount = 0;    //gets calculated
 var sDelayCount = 0;
 var activeEnemies = 0;   //gets calculated
@@ -48,8 +48,9 @@ var choosing = false;    //gets calculated
 var choosingFor = null   //gets calculated
 var subtypes = ["basic","slow"]
 var options = [] //gets calculated
-var prices = [] //gets calculated
+var prices = new Array(10).fill(0) //gets calculated
 var popUpAlpha = 0.7 
+var startPrices = [20,5]
 
 //Main Animation Loop
 const mainLoop = function(){
@@ -62,7 +63,6 @@ const mainLoop = function(){
 
     if(gameActive){
         drawBoard()
-
         if(waveRunning){
             activateEnemy()
         }
@@ -193,22 +193,6 @@ const drawPopup = function(){
     }
     if(choosing){
         fillRec(popUpRec,colText([0,0,0,0.75]))
-        
-        
-        options = []
-        if(choosingFor.type =="empty"){
-            options = [...subtypes]
-        }else if(choosingFor.type == "base"){
-            options = ["upgrade1", "upgrade2", "upgrade3", "repair"]
-        }else if(choosingFor.type == "defense"){
-            options = ["upgrade1", "upgrade2", "upgrade3"]
-            for(var i=0; i<subtypes.length; i++){
-                if(subtypes[i] != choosingFor.subtype){
-                    options.push(subtypes[i])
-                }
-            }
-        }
-
 
         sellButtonRec = [popUpRec[0],popUpRec[1]+popUpRec[3]*0.8,popUpRec[2]/2.2, popUpRec[3]*0.2]
         fillRec(sellButtonRec,colText([100,100,100,popUpAlpha]))
@@ -262,6 +246,53 @@ const checkRelease = function(){
                 console.log("opening options for",sPiece)
                 choosingFor = sPiece
                 waveRunning = false;
+
+
+                options = []
+                if(choosingFor.type =="empty"){
+                    options = [...subtypes]
+                }else if(choosingFor.type == "base"){
+                    options = ["upgrade1", "upgrade2", "upgrade3", "repair"]
+                }else if(choosingFor.type == "defense"){
+                    options = ["upgrade1", "upgrade2", "upgrade3"]
+                    for(var i=0; i<subtypes.length; i++){
+                        if(subtypes[i] != choosingFor.subtype){
+                            options.push(subtypes[i])
+                        }
+                    }
+                }
+                
+                for (var i = 0; i< options.length; i++){
+                    switch(options[i]){
+                        case "upgrade1":
+                            prices[i] = choosingFor.cost
+                            break;
+                        case "upgrade2":
+                            prices[i] = floor(choosingFor.cost/2)
+                            break;
+                        case "upgrade3":
+                            prices[i] = floor(choosingFor.cost*1.5)
+                            break;
+                        case "slow":
+                            prices[i] = (choosingFor.type == "empty")? startPrices[1] : floor(choosingFor.cost)
+                            
+                            break;
+                        case "basic":
+                            prices[i] = (choosingFor.type == "empty")? startPrices[0] : floor(choosingFor.cost)
+                            break;  
+                        case "repair":
+                            prices[i] = 100;
+                            var maxPrice = prices[i]
+                            maxPrice *= (100-theBase.health)/100
+                            maxPrice = ceil(maxPrice )
+                            if(maxPrice > gold){maxPrice = gold}
+                            prices[i] = maxPrice
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
                 
             }
             sPiece.isSelected = false;
@@ -378,6 +409,12 @@ const click = function(){
                 options=[]
                 waveRunning = true;
             }
+            
+            console.log(
+                "calculating costs"
+            )
+            
+
             for(var i=0; i<options.length; i++){
                 if(isInside([mdX,mdY],optionsRecs[i])){
                     console.log('option '+i.toString()+" clicked")
@@ -385,16 +422,18 @@ const click = function(){
 
                     if(gold >= thePrice){
                         if(choosingFor.type == "empty"){
+
+
                             if(options[i] == "basic"){
                                 choosingFor.type = "defense"
                                 choosingFor.subtype = "basic"
-                                choosingFor.cost = thePrice
+                                choosingFor.cost = startPrices[0]
                                 theDefences.push(choosingFor)
                             }else if(options[i] == "slow"){
                                 choosingFor.type = "defense"
                                 choosingFor.subtype = "slow"
                                 choosingFor.numLasers = 3;
-                                choosingFor.cost = thePrice
+                                choosingFor.cost = startPrices[1]
                                 theDefences.push(choosingFor)
                             }
                         }else if(choosingFor.type == "defense"){
@@ -404,9 +443,9 @@ const click = function(){
                                 console.log("dfghmfghjmghk,")
                             }else if(options[i] == "upgrade2"){
                                 if(choosingFor.subtype == "slow"){
-                                    choosingFor.damage/=2
+                                    choosingFor.power/=2
                                 }else{
-                                    choosingFor.damage+=0.25
+                                    choosingFor.power++
                                 }
                                 choosingFor.cost += thePrice
                             }else if(options[i] == "upgrade3"){
@@ -430,7 +469,7 @@ const click = function(){
                                 choosingFor.numLasers ++
                                 choosingFor.cost += thePrice
                             }else if(options[i] == "upgrade2"){
-                                choosingFor.damage++
+                                choosingFor.power++
                                 choosingFor.cost += thePrice
                             }else if(options[i] == "upgrade3"){
                                 choosingFor.range++
@@ -459,229 +498,6 @@ const click = function(){
     }
 }
 
-class Piece {
-    constructor(i, j, type) {
-        this.type = type
-        this.cost = 20
-        this.subtype = "basic"
-        this.color = [40,40,40]
-        this.left = 0;
-        this.top = 0;
-        this.px = i;
-        this.py = j;
-        this.isSelected = false;
-        this.isTarget = false;
-        this.health = 100;
-        this.enemies = new Array([-1]);
-        this.range = 4;
-        this.numLasers = 1;
-        this.damage = 0.25;
-        this.level = 1;
-
-        this.draw = () => {
-            
-            this.color = (this.type=="base")? [126*(1-this.health/100),0,123*this.health/100] : (this.type=="defense")? [160,160,160] : (this.type=="path")? [123, 123, 123]  : [40,40,40]
-            var col = this.isSelected? [0,123,0] : this.isTarget? [123,123,0] : this.color
-            
-            fillRec([this.left, this.top, pieceSize, pieceSize], colText(col));
-            
-            if(this.type == "base"){
-                fillRec([this.left, this.top+pieceSize*0.9, pieceSize*this.health/100, pieceSize*0.1], colText([123,255,0]))
-                // strokeCir([this.left+pieceSize/2, this.top+pieceSize/2, pieceSize*this.range], 3, colText(col))
-
-            }
-            if(this.type == "defense"){
-                var col = (this.subtype == "basic")? [100,100,0] : (this.subtype == "slow")? [0,100,100] : [255,255,255]
-                fillCir([this.left+pieceSize/2, this.top+pieceSize/2, pieceSize*0.75/2], colText(col))
-                // strokeCir([this.left+pieceSize/2, this.top+pieceSize/2, pieceSize*this.range], 3, colText(col))
-            }
-        }
-        this.drawLaser = ()=>{
-            //todo find nearest enemy and qualify
-            //todo draw line to enemy
-            this.enemies = new Array([-1])
-
-            
-            var sor = [this.left+pieceSize/2, this.top + pieceSize/2]
-            
-            for(var li = 0; li<this.numLasers; li++){
-                var tarPind = 1000000
-                var closest = 1000000;
-                for(var ei=(activeEnemies - 1); ei>=0; ei--){
-                    if(!this.enemies.includes(ei)){
-                        var tartar = [theEnemies[ei].left+pieceSize/4, theEnemies[ei].top + pieceSize/4]
-                        var dis = dist(sor,tartar)
-                        
-                        if(dis <= this.range*pieceSize){
-                            //enemy found
-                            if(this.subtype == "slow"){
-                                if(dis<closest){
-                                    closest = dis
-                                    this.enemies[li] = ei
-                                    // console.log(dis, this.range, pieceSize)
-
-                                }
-                            }else{
-                                if(tarPind > theEnemies[ei].pathStep){
-                                    this.enemies[li] = ei
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if(this.enemies[0] > -1){
-
-                for(var ei=this.enemies.length -1; ei>=0; ei--){
-                    if(this.enemies[ei]<0){
-                        console.warn(this.enemies[ei])
-                    }else if(this.enemies[ei] > (theEnemies.length-1)){
-                        console.warn(this.enemies[ei])
-                    }
-                    var theenemy = theEnemies[this.enemies[ei]]
-                    var tar = [theenemy.left+pieceSize/4, theenemy.top + pieceSize/4]
-                    var sz = 3; var co = [200,180,0,0.8]
-                    if(this.type == "base"){
-                        sz = 5; co = [200,123,50,0.8]
-                    }
-                    if(this.subtype == "slow"){
-                        co = [0,200,200,0.8]
-                    }
-                    drawLine(sor,tar,sz,colText(co))
-                    
-                    if(this.subtype == "basic"){
-                        if(theenemy.health > this.damage){
-                            theenemy.health-=this.damage
-                        }else{
-                            theenemy.health = 0
-                            theenemy.destroy()
-                            score+=theenemy.reward
-                            gold+=theenemy.reward
-                            if(ei > 0){
-                                this.enemies.pop()
-                            }else{
-                                this.enemies[0] = -1
-                            }
-                        }
-                    }else if(this.subtype == "slow"){
-                        theenemy.speedMod *=this.damage;
-                    }
-                }
-                
-                
-
-            }
-        }
-    }
-}
-
-class Enemy { 
-    constructor(type, pathnum){
-        this.type = type
-        this.color = [123,0,0]
-        this.speed = 0.03;
-        this.left = 50;
-        this.top = 50;
-        this.from = [-1,-1]
-        this.to  = [-1,-1]
-        this.health = 100;
-        this.damage = 10; 
-        this.pathNum = pathnum;
-        this.reward = 1;
-        // console.log(paths)
-        // console.log(pathnum)
-        this.pathStep = paths[pathnum].length -1
-        this.visible = false;
-        this.speedMod = 1;
-
-        this.draw = ()=>{
-            if((this.to[0]<0)||(this.from[0]<0)){
-                if(this.visible){
-                    this.visible = false
-                }
-            }else{
-                this.visible = true;
-            }
-
-            if(this.visible){
-                fillRec([this.left, this.top, pieceSize/2, pieceSize/2], colText(this.color));
-                fillRec([this.left, this.top+pieceSize/2*0.9, (pieceSize/2)*this.health/100, pieceSize/2*0.1], colText([123,255,0]))
-            }
-
-        }
-        this.move = ()=>{
-            var dir = [ this.to[0]-this.from[0] , this.to[1] - this.from[1]  ]
-            var spDir = dir
-            if(verticalOrien){
-                spDir = [ -spDir[1], spDir[0]]
-            }
-
-            this.left= this.left + this.speed*spDir[0]*pieceSize*this.speedMod
-            this.top= this.top + this.speed*spDir[1]*pieceSize*this.speedMod
-            this.speedMod = 1;
-
-            if(this.to[0]>-1){
-                var destPiece = gameGrid[this.to[0]][this.to[1]]
-                var pastLeft = spDir[0]*((destPiece.left + pieceSize/2) - (this.left + pieceSize/4)) <= 0
-                var pastTop = spDir[1]*((destPiece.top + pieceSize/2) - (this.top + pieceSize/4)) <= 0
-
-                if(pastLeft){
-                    this.left = destPiece.left + pieceSize/4
-                }
-                if(pastTop){
-                    this.top = destPiece.top + pieceSize/4
-                }
-
-                if(pastLeft&&pastTop){
-        
-                    this.pathStep = this.pathStep - 1;
-                    if(this.pathStep < - 0){
-                        //hit base
-                        if(theBase.health > this.damage){
-                            theBase.health-= this.damage
-                        }else{
-                            theBase.health = 0
-                            waveRunning = false;
-                            gameOver = true;
-
-                        }
-
-
-                        this.destroy()
-                    }else{
-                        
-                        this.from = [...this.to]
-                        this.to = [...paths[this.pathNum][this.pathStep]]
-                    }
-                    
-                }
-                // console.log('moving', dir, this.speed, this.left, this.top)
-            }
-            
-        }
-        this.destroy = ()=>{
-            this.speed = 0;
-            this.to = [-1,-1]
-            this.from = [-1,-1]
-            this.left = 0;
-            this.top = 0;
-            activeEnemies--
-            theEnemies.splice(theEnemies.indexOf(this),1)
-            // console.log("enemy removed", theEnemies)
-            if(theEnemies.length == 0){
-                waveNum++
-                genEnemies()
-                eDelayCount = 0
-                sDelayCount = 0;
-
-            }
-        }
-
-    }
-}
-
-
 const genGrid = function(){
 
     console.log('generating new grid')
@@ -707,8 +523,7 @@ const genGrid = function(){
     
     theBase = gameGrid[baseP[0]][baseP[1]]
     theBase.type = "base"
-    theBase.damage = 1;
-    theBase.range = 8
+    theBase.cost = 100
     //todo generate paths
     
     for(var p=0; p<numPaths; p++){
@@ -857,7 +672,7 @@ const drawTexts = function(){
             var numLText = choosingFor.numLasers.toString()
             
             //power
-            var powText = choosingFor.damage.toString()
+            var powText = choosingFor.power.toString()
 
             //range
             var ranText = choosingFor.range.toString()
@@ -903,17 +718,8 @@ const drawTexts = function(){
                 
                 fillText(opRec[0]+opRec[2]/2, opRec[1], optionText, textH*0.5, "white")
 
-                prices[i] = 20;
-                if(options[i]=="repair"){
-                    prices[i] = 100;
-                    var maxPrice = prices[i]
-                    maxPrice *= (100-theBase.health)/100
-                    maxPrice = ceil(maxPrice )
-                    if(maxPrice > gold){maxPrice = gold}
-                    prices[i] = maxPrice
-                }else if(options[i]=="slow"){
-                    prices[i] = prices[i]/4
-                }
+                
+
                 var optionText2 = prices[i].toString() + " G"
 
                 fillText(opRec[0]+opRec[2]/2, opRec[1]+opRec[3]/8, optionText2, textH*0.5, "white")
